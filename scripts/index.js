3bf528d2a24dbb1df62e363d1fb31f14448e85ef
Vlad Tsoy <potachekkk@yandex.ru>
2023-04-30 19:54:32 +0300

    feature: добавить мобильную версию

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,27 @@ const popupOpenButton = document.querySelector('.profile__popup-opened');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 
-popupOpenButton.addEventListener('click', () => {
-    popup.classList.add('popup__opened')
-})
-popupCloseButton.addEventListener('click', () => {
-    popup.classList.remove('popup__opened')
-})
+const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened')
 
-const formElement = document.querySelector('.popup__form');
+popupOpenButton.addEventListener('click', () => togglePopupState(popup));
+
+popupCloseButton.addEventListener('click', () => togglePopupState(popup));
+
+const formElement = document.querySelector('.popup__form_type_profile');
 const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
 const ProfileInfoTitle = document.querySelector('.profile__info_title');
 const ProfileInfoSubtitle = document.querySelector('.profile__info_subtitle');
 
+
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
-    // nameInput = document.getElementById('name').value;
-    // aboutInput = document.getElementById('about').value;
+    nameInput = document.getElementById('name').value;
+    aboutInput = document.getElementById('about').value;
 
-    ProfileInfoTitle.textContent = nameInput.value;
-    ProfileInfoSubtitle.textContent = aboutInput.value;
+    ProfileInfoTitle.textContent = `${nameInput}`;
+    ProfileInfoSubtitle.textContent = `${aboutInput}`;
     
+    togglePopupState(popup);
 }
