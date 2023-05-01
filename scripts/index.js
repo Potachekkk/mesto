805bba998a4b1002ac631d67fc368ce08d38b1fd
Vlad Tsoy <potachekkk@yandex.ru>
2023-05-01 14:39:34 +0300

    feature: добафить функционал сабмит

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,27 @@
 const popupOpenButton = document.querySelector('.profile__popup-opened');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
-
 const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened')
+const ProfileInfoTitle = document.querySelector('.profile__info_title');
+const ProfileInfoSubtitle = document.querySelector('.profile__info_subtitle');
 
 popupOpenButton.addEventListener('click', () => togglePopupState(popup));
 
 popupCloseButton.addEventListener('click', () => togglePopupState(popup));
 
+
+
 const formElement = document.querySelector('.popup__form_type_profile');
 const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
-const ProfileInfoTitle = document.querySelector('.profile__info_title');
-const ProfileInfoSubtitle = document.querySelector('.profile__info_subtitle');
 
+
+formElement.addEventListener('submit', handleFormSubmit)
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
+    ProfileInfoTitle.textContent = nameInput.value;
+    ProfileInfoSubtitle.textContent = aboutInput.value;
 
-    nameInput = document.getElementById('name').value;
-    aboutInput = document.getElementById('about').value;
-
-    ProfileInfoTitle.textContent = `${nameInput}`;
-    ProfileInfoSubtitle.textContent = `${aboutInput}`;
-    
     togglePopupState(popup);
-}
+};
