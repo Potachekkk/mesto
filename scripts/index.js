4acc71f540305d260fd16e9cf52c901425b0fbcb
Vlad Tsoy <potachekkk@yandex.ru>
2023-05-02 14:48:21 +0300

    feature: добавить мобильный эдит

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,17 @@ const popupCloseButton = document.querySelector('.popup__close');
 const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened')
 const ProfileInfoTitle = document.querySelector('.profile__info_title');
 const ProfileInfoSubtitle = document.querySelector('.profile__info_subtitle');
-
-popupOpenButton.addEventListener('click', () => togglePopupState(popup));
-
-popupCloseButton.addEventListener('click', () => togglePopupState(popup));
-
-
-
 const formElement = document.querySelector('.popup__form_type_profile');
 const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
 
+popupOpenButton.addEventListener('click', () => togglePopupState(popup),
+nameInput.value = ProfileInfoTitle.textContent,
+aboutInput.value = ProfileInfoSubtitle.textContent);
+
+popupCloseButton.addEventListener('click', () => togglePopupState(popup));
 
 formElement.addEventListener('submit', handleFormSubmit)
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
     ProfileInfoTitle.textContent = nameInput.value;
