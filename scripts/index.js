84cc080d8cfee8b99c8f20ad6c590b6037c00e37
Vlad Tsoy <potachekkk@yandex.ru>
2023-05-03 00:25:58 +0300

    feature: добавить popup инпуты

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 const popupOpenButton = document.querySelector('.profile__popup-opened');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
+
 const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened')
-const ProfileInfoTitle = document.querySelector('.profile__info_title');
-const ProfileInfoSubtitle = document.querySelector('.profile__info_subtitle');
-const formElement = document.querySelector('.popup__form_type_profile');
+
 const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
+const ProfileInfoTitle = document.querySelector('.profile__info_title');
+const ProfileInfoSubtitle = document.querySelector('.profile__info_subtitle');
+const PopupSaveButton = document.querySelector('.popup__save');
 
 popupOpenButton.addEventListener('click', () => togglePopupState(popup),
 nameInput.value = ProfileInfoTitle.textContent,
@@ -14,7 +16,9 @@ aboutInput.value = ProfileInfoSubtitle.textContent);
 
 popupCloseButton.addEventListener('click', () => togglePopupState(popup));
 
-formElement.addEventListener('submit', handleFormSubmit)
+const formElement = document.querySelector('.popup__form_type_profile');
+
+formElement.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit (evt) {
     evt.preventDefault();
     ProfileInfoTitle.textContent = nameInput.value;
