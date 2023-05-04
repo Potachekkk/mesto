93e9222046b774ed17b4e1af431f92dc4b91b397
Vlad Tsoy <potachekkk@yandex.ru>
2023-05-04 19:10:44 +0300

    refactor r

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
 const popupOpenButton = document.querySelector('.profile__popup-opened');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
-
-const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened')
-
 const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
-const ProfileInfoTitle = document.querySelector('.profile__title');
-const ProfileInfoSubtitle = document.querySelector('.profile__subtitle');
-const PopupSaveButton = document.querySelector('.popup__save');
-
-popupOpenButton.addEventListener('click', () => togglePopupState(popup),
-nameInput.value = ProfileInfoTitle.textContent,
-aboutInput.value = ProfileInfoSubtitle.textContent);
-
-popupCloseButton.addEventListener('click', () => togglePopupState(popup));
-
+const profileInfoTitle = document.querySelector('.profile__title');
+const profileInfoSubtitle = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.popup__form_type_profile');
 
-formElement.addEventListener('submit', handleFormSubmit);
+const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened')
+
+// обработчик отправки формы
 function handleFormSubmit (evt) {
-    evt.preventDefault();
-    ProfileInfoTitle.textContent = nameInput.value;
-    ProfileInfoSubtitle.textContent = aboutInput.value;
+    evt.preventDefault(); // отменяем стандартную форму
+    profileInfoTitle.textContent = nameInput.value;
+    profileInfoSubtitle.textContent = aboutInput.value;
 
     togglePopupState(popup);
 };
+
+// открываем попап
+popupOpenButton.addEventListener('click', () => togglePopupState(popup),
+nameInput.value = profileInfoTitle.textContent,
+aboutInput.value = profileInfoSubtitle.textContent);
+
+// закрываем попап
+popupCloseButton.addEventListener('click', () => togglePopupState(popup));
+
+// отправляем форму
+formElement.addEventListener('submit', handleFormSubmit);
