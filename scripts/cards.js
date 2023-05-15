95a516c249b10bb2fdce541d9a7c4d6b4b4a3e4e
Влад Цой <potachekkk@yandex.ru>
2023-05-15 17:37:51 +0300

    123

---
+++ scripts/cards.js
@@ -0,0 +1,38 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+const cardsElement = document.querySelector('.elements__container');
+const templateCards = document.querySelector('.elements__item').content;
+
+initialCards.forEach((item) => {
+    const itemElement = templateCards.cloneNode(true);
+    itemElement.querySelector('.element__image').src = item.link;
+    itemElement.querySelector('.element__title').textContent = item.name;
+    cardsElement.append(itemElement)
+
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
-const popupOpenButton = document.querySelector('.profile__popup-opened');
-const popup = document.querySelector('.popup');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
+const popupOpenAddButton = document.querySelector('.profile__add-button');
+const popupCloseAddButton = document.querySelector('.popup__close');
+
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupAdd = document.querySelector('.popup_type_add');
+
 const nameInput = document.querySelector('.popup__input_type_name');
 const aboutInput = document.querySelector('.popup__input_type_about');
 const profileInfoTitle = document.querySelector('.profile__title');
@@ -15,16 +20,25 @@ function handleFormSubmit (evt) {
     profileInfoTitle.textContent = nameInput.value;
     profileInfoSubtitle.textContent = aboutInput.value;
 
-    togglePopupState(popup);
+    togglePopupState(popupProfile);
 };
 
 // открываем попап
-popupOpenButton.addEventListener('click', () => togglePopupState(popup),
+// popupOpenButton.addEventListener('click', () => togglePopupState(popupProfile),
+popupOpenButton.addEventListener('click', () => popupProfile.classList.add('popup_opened')),
 nameInput.value = profileInfoTitle.textContent,
-aboutInput.value = profileInfoSubtitle.textContent);
+aboutInput.value = profileInfoSubtitle.textContent
+;
+// popupOpenAddButton.addEventListener('click', () => togglePopupState(popupAdd))
+popupOpenAddButton.addEventListener('click', () => popupAdd.classList.add('popup_opened'))
 
 // закрываем попап
-popupCloseButton.addEventListener('click', () => togglePopupState(popup));
+// popupCloseButton.addEventListener('click', () => togglePopupState(popupProfile));
+popupCloseButton.addEventListener('click', () => popupProfile.classList.remove('popup_opened'));
+popupCloseAddButton.addEventListener('click', () => popupAdd.classList.remove('popup_opened'));
+console.log(popupAdd.classList)
+
+// popupCloseAddButton.addEventListener('click', () => togglePopupState(popupAdd));
 
 // отправляем форму
 formElement.addEventListener('submit', handleFormSubmit);
