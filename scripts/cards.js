8e200ccc1d3b48767b0e17cf299f15b8e5f35c24
Влад Цой <potachekkk@yandex.ru>
2023-06-19 14:08:48 +0300

    refactor card

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,52 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+
+class Card {
+  constructor(name, link, openPopupPic, templateSelector) {
+    this._name = name;
+    this._link = link;
+    this._openPopupPic = openPopupPic;
+    this._templateSelector = templateSelector;
+  }
+
+  _getTemplate() {
+    const templateCards = document.querySelector(this._templateSelector).content;
+    const itemElement = templateCards.cloneNode(true);
+    return itemElement;
+  }
+
+  _openItem() {
+    this._openPopupPic(this._name, this._link);
+  }
+
+  _deleteItem() {
+    this._element.remove()
+  }
+
+  _likeItem() {
+  evt.target.classList.toggle("element__like-button_active");
+    this._element.querySelector('.element__like-button').classList.toggle('element__like-button_active')
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    const elementImage = itemElement.querySelector(".element__image");
+    elementImage.src = this._link;
+    elementImage.alt = `Изображение места ${this._name}`;
+    this._element.querySelector('.popup__figcaption').textContent = this._name;
+    this._setEventListeners();
+    return this._element;
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.element__delete-button').addEventListener('click', () => {
+      this._deleteItem();
+    })
+    this._element.querySelector('.element__like-button').addEventListener('click', () => {
+      this._likeItem();
+    })
+    this._element.querySelector('.element__image').addEventListener('click', () => {
+      this._openItem();
+  })
+}}
